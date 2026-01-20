f97ce7e70861db260a0afe8bef34205c4f7bd668
reddoandy <shuntian1234567890@gmail.com>
2026-01-20 18:00:02 +0800

    init

---
+++ GameBackend/Controllers/MatchController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MatchController : ControllerBase
+    {
+
+        [HttpPost("join")]
+        public IActionResult JoinMatch([FromBody] MatchRequest request)
+        {
+            Console.WriteLine($"收到UE傳來的EOS ID:{request.EosId}");
+
+            return Ok(new
+            {
+                status = "QUEUED"
+            });
+        }
+    }
+
+        public class MatchRequest
+        {
+            public string EosId {  get; set; }
+        }
+    
+}
